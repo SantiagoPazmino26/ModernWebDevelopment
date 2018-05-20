d87499c40805419f484d60ee0e1736d2f20578ae
Santiago Pazmino <blacksantzero@gmail.com>
2018-05-20 17:29:43 +0200

    Project config

---
+++ scripts/start.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+BABEL_ENV=backend npx babel-node src/backend/index.js & backendPid=$!
+
+npx webpack-dev-server --mode development --open & devServerPid=$!
+
+killBackend() {
+		if kill -9 ${backendPid}
+		then
+			echo "stopped backend server"
+		fi
+	}
+
+killDevServer(){
+	if kill -9 ${devServerPid}
+	then
+		echo "stopped webpack dev server"
+	fi
+}
+
+onClose() {
+	killDevServer
+	killBackend
+}
+
+if ! kill -0 ${devServerPid}
+then
+	killBackend
+	exit 1
+fi
+
+if ! kill -0 ${backendPid}
+then
+	killDevServer
+	exit 1
+fi
+
+trap onClose 1 2 3 6
+wait
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const path require('path')
+const appModulePath = path.resolve(__dirname, 'src')
+
+module.exports = {
+	entry: './src/app/index.jsx',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js',
+	},
+	module:{
+		rules: [
+			{
+				test: /\.jsx?$/,
+				use: 'babel-loader',
+				include: [appModulePath]
+			},
+			{
+				test:/\.css$/,
+				use: ['style-loader','css-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.iscss$/,
+				use: ['inline-css-webpack-loader']
+			}
+			],
+		},
+		resolve: {
+			modules: [appModulePath, 'node-modules'],
+			extensions: ['.js', '.jsx', '.json']
+		},
+		plugins:[
+			new HtmlWebpackPlugin({template: __dirname + '/index.html'}),
+			new CleanWebpackPlugin(['dist']),
+			],
+		devtool; 'source-map',
+		devserver: {
+			proxy: {
+				"/api": {
+					target: "http://localhost:3000/api",
+					pathRewrite: {"^/api": ""},
+				}
+			}
+		}
+} 
+
+
+
